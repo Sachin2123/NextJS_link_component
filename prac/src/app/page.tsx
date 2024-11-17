29f3e8c8e79e86c99e7b06712a518ed8e4a8a723
Sachin2123 <sachinvishwakarma8286@gmail.com>
2024-11-17 20:43:31 +0530

    add new

--- prac/src/app/page.tsx
+++ prac/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+// sachin
 export default function Home() {
   const val = [{ name: "Sachin" }, { name: "Dhoni" }];
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Link href="/about">About</Link>
 
       {val.map((item) => (
-         <h1>{item.name}</h1>
+        <h1>{item.name}</h1>
       ))}
     </div>
   );
