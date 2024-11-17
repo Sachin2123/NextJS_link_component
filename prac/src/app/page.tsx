e22fa1a880944684ee4cfe567724ea40b5940814
Sachin2123 <sachinvishwakarma8286@gmail.com>
2024-11-17 20:41:41 +0530

    first commit

---
+++ prac/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  const val = [{ name: "Sachin" }, { name: "Dhoni" }];
+  return (
+    <div>
+      <Link href="/about">About</Link>
+
+      {val.map((item) => (
+         <h1>{item.name}</h1>
+      ))}
+    </div>
+  );
+}
